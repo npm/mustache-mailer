2f7cd083b0dfcff75062deb9de56d665ac973420
bcoe <bencoe@gmail.com>
2015-02-17 18:09:33 -0800

    added support for token generation, and for meta information.

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 var _ = require('lodash'),
   fs = require('fs'),
   Handlebars = require('handlebars'),
+  handlebarsAsync = require('handlebars-async'),
   nodemailer = require('nodemailer'),
   path = require('path'),
   Promise = require('bluebird');
+
+handlebarsAsync(Handlebars);
 
 // the message objects returned by
 // .message()
@@ -16,18 +19,44 @@ Message.prototype.sendMail = function(data, cb) {
   var _this = this,
     content = {};
 
-  return new Promise(function(resolve, reject) {
-    if (_this.templates.html) content.html = _this.templates.html(data);
-    if (_this.templates.text) content.text = _this.templates.text(data);
+  this._expandTemplate(data, _this.templates.html)
+    .then(function(rendered) {
+      if (rendered) content.html = rendered;
+      return _this._expandTemplate(data, _this.templates.text)
+    })
+    .then(function(rendered) {
+      if (rendered) content.text = rendered;
+      return _this._expandTemplate(data, _this.templates.meta)
+    })
+    .then(function(meta) {
+      if (meta) return JSON.parse(meta);
+      else return {};
+    })
+    .then(function(meta) {
+      return new Promise(function(resolve, reject) {
+        _this.transporter.sendMail(_.extend({}, content, meta, data), function(err, info) {
+          if (err) reject(err);
+          else resolve(info);
+        });
+      });
+    })
+    .nodeify(cb);
+};
 
-    _this.transporter.sendMail(_.extend({}, content, data), function(err, info) {
+Message.prototype._expandTemplate = function(data, template) {
+  if (!template) return Promise.cast(null);
+
+  return new Promise(function(resolve, reject) {
+    template(data, function(err, content) {
       if (err) reject(err);
-      else resolve(info);
+      else resolve(content);
     });
-  }).nodeify(cb);
+  });
 };
 
 function MustacheMailer(opts) {
+  var _this = this;
+
   _.extend(this, {
     nodemailer: {}, // node-mailer initialization options.
     transport: null, // the transport method, e.g., SES.
@@ -36,12 +65,25 @@ function MustacheMailer(opts) {
   }, opts);
 
   this.transporter = nodemailer.createTransport(this.transport);
+
+  // if we provide a helper for generating tokens, e.g.,
+  // email signup tokens, register the async helper.
+  if (this.tokenFacilitator) {
+    Handlebars.registerHelper('tokenHelper', function(data) {
+      var done = this.async();
+      _this.tokenFacilitator.generate(data.hash, function(err, token) {
+        if (err) return done(err);
+        else return done(null, token);
+      });
+    });
+  }
 }
 
 MustacheMailer.prototype.message = function(name, cb) {
   var _this = this,
     templates = {},
     htmlPath,
+    metaPath,
     textPath;
 
   if (_this.cache[name]) {
@@ -52,6 +94,7 @@ MustacheMailer.prototype.message = function(name, cb) {
       .then(function(files) {
         htmlPath = _this._resolveTemplateFile(name + '.html.hbs', files);
         textPath = _this._resolveTemplateFile(name + '.text.hbs', files);
+        metaPath = _this._resolveTemplateFile(name + '.meta.hbs', files);
 
         if (textPath) return _this._loadTemplate(textPath);
       })
@@ -61,6 +104,10 @@ MustacheMailer.prototype.message = function(name, cb) {
       })
       .then(function(htmlTemplate) {
         if (htmlTemplate) templates.html = htmlTemplate;
+        if (metaPath) return _this._loadTemplate(metaPath);
+      })
+      .then(function(metaTemplate) {
+        if (metaTemplate) templates.meta = metaTemplate;
       })
       .then(function() {
         var message = new Message(_this.transporter, templates);
