ce2e23e3b48557208f25463bb3484809411b741d
C J Silverio <ceejceej@gmail.com>
2016-11-04 17:30:48 -0700

    feat: missing templates now throw errors.
    Rewrote message creation to be clearer & to fix this bug. Wrote a
    test to verify this.
    
    Rewrote chunks of sendMail to use promises as eventual values, not
    flow control.
    
    Updated dependencies. Fixed bugs caused by changes in lodash.

--- index.js
+++ index.js
@@ -4,55 +4,53 @@ var _ = require('lodash'),
   handlebarsAsync = require('handlebars-async'),
   nodemailer = require('nodemailer'),
   path = require('path'),
-  Promise = require('bluebird');
+  P = require('bluebird');
 
 handlebarsAsync(Handlebars);
 
-// the message objects returned by
-// .message()
+// the message objects returned by .message()
 function Message(transporter, templates) {
   this.transporter = transporter;
   this.templates = templates;
 }
 
-Message.prototype.sendMail = function(data, cb) {
-  var _this = this,
-    content = {};
+Message.prototype.sendMail = function sendMail(data, callback) {
+  var content = {},
+    self = this;
 
-  return this._expandTemplate(data, _this.templates.html)
-    .then(function(rendered) {
-      if (rendered) content.html = rendered;
-      return _this._expandTemplate(data, _this.templates.text)
-    })
-    .then(function(rendered) {
-      if (rendered) content.text = rendered;
-      return _this._expandTemplate(data, _this.templates.meta)
-    })
-    .then(function(meta) {
-      if (meta) return JSON.parse(meta);
-      else return {};
-    })
-    .then(function(meta) {
-      return new Promise(function(resolve, reject) {
-        // first copy content and override with data
-        var mail = _.extend({}, content, data);
-        // then override with meta if it has a truthy value
-        _.extend(mail, meta, function (mailValue, metaValue) {
-          return metaValue || mailValue;
-        });
-        _this.transporter.sendMail(mail, function(err, info) {
-          if (err) reject(err);
-          else resolve(info);
-        });
+  const getRendered = this._expandTemplate(data, this.templates.html);
+  const getText = this._expandTemplate(data, this.templates.text);
+
+  const getMeta = this._expandTemplate(data, this.templates.meta)
+  .then(function(meta) {
+      meta = meta ? JSON.parse(meta) : {};
+      Object.keys(meta).forEach(k => {
+        if (!meta[k]) delete meta[k];
       });
-    })
-    .nodeify(cb);
-};
+      return meta;
+  });
+
+  return P.join(getRendered, getMeta, getText, function renderTmpl(rendered, meta, text) {
+
+    content.text = text;
+    content.html = rendered;
+    var mail = Object.assign({}, content, data, meta);
+
+    var deferred = P.defer();
+    self.transporter.sendMail(mail, function(err, info) {
+      if (err) deferred.reject(err);
+      else deferred.resolve(info);
+    });
+
+    return deferred.promise;
+  })
+  .nodeify(callback);
+}
 
 Message.prototype._expandTemplate = function(data, template) {
-  if (!template) return Promise.cast(null);
+  if (!template) return P.cast(null);
 
-  return new Promise(function(resolve, reject) {
+  return new P(function(resolve, reject) {
     template(data, function(err, content) {
       if (err) reject(err);
       else resolve(content);
@@ -82,7 +80,7 @@ function MustacheMailer(opts) {
         _.pick(data.hash, ['prefix', 'ttl']),
         function(err, token) {
           if (err) return done(err);
-          else return done(null, token);
+          return done(null, token);
         }
       );
     });
@@ -97,35 +95,35 @@ MustacheMailer.prototype.message = function(name, cb) {
     textPath;
 
   if (_this.cache[name]) {
-    return Promise.cast(_this.cache[name])
-      .nodeify(cb);
-  } else {
-    return this._templateList()
-      .then(function(files) {
-        htmlPath = _this._resolveTemplateFile(name + '.html.hbs', files);
-        textPath = _this._resolveTemplateFile(name + '.text.hbs', files);
-        metaPath = _this._resolveTemplateFile(name + '.meta.hbs', files);
-
-        if (textPath) return _this._loadTemplate(textPath);
-      })
-      .then(function(textTemplate) {
-        if (textTemplate) templates.text = textTemplate;
-        if (htmlPath) return _this._loadTemplate(htmlPath);
-      })
-      .then(function(htmlTemplate) {
-        if (htmlTemplate) templates.html = htmlTemplate;
-        if (metaPath) return _this._loadTemplate(metaPath);
-      })
-      .then(function(metaTemplate) {
-        if (metaTemplate) templates.meta = metaTemplate;
-      })
-      .then(function() {
-        var message = new Message(_this.transporter, templates);
-        _this.cache[name] = message;
-        return message;
-      })
-      .nodeify(cb);
+    return P.cast(_this.cache[name]).nodeify(cb);
   }
+
+  return this._templateList()
+  .then(function(files) {
+    htmlPath = _this._resolveTemplateFile(name + '.html.hbs', files);
+    textPath = _this._resolveTemplateFile(name + '.text.hbs', files);
+    metaPath = _this._resolveTemplateFile(name + '.meta.hbs', files);
+
+    const loadText = _this._loadTemplate(textPath);
+    const loadHTML = _this._loadTemplate(htmlPath);
+    const loadMeta = _this._loadTemplate(metaPath);
+
+    return P.join(loadText, loadHTML, loadMeta);
+  })
+  .spread(function(textT, htmlT, metaT) {
+    if (textT) templates.text = textT;
+    if (htmlT) templates.html = htmlT;
+    if (metaT) templates.meta = metaT;
+
+    if (!templates.text && !templates.html) {
+      throw new Error('template not found');
+    }
+
+    var message = new Message(_this.transporter, templates);
+    _this.cache[name] = message;
+    return message;
+  })
+  .nodeify(cb);
 };
 
 MustacheMailer.prototype._resolveTemplateFile = function(name, files) {
@@ -133,9 +131,9 @@ MustacheMailer.prototype._resolveTemplateFile = function(name, files) {
 };
 
 MustacheMailer.prototype._loadTemplate = function(path) {
-  var _this = this;
+  if (!path) return P.cast(null);
 
-  return new Promise(function(resolve, reject) {
+  return new P(function(resolve, reject) {
     fs.readFile(path, 'utf-8', function(err, source) {
       if (err) reject(err);
       else resolve(Handlebars.compile(source));
@@ -146,7 +144,7 @@ MustacheMailer.prototype._loadTemplate = function(path) {
 MustacheMailer.prototype._templateList = function() {
   var _this = this;
 
-  return new Promise(function(resolve, reject) {
+  return new P(function(resolve, reject) {
     fs.readdir(_this.templateDir, function(err, files) {
       if (err) reject(err);
       else resolve(
